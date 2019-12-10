ae89eb84d644afd1b6829382703ca719d2605808
legaz <45671425+legaz@users.noreply.github.com>
2019-12-10 13:50:43 +0100

    Update Sql_ICG_projekat_Danilo_Radulovic.sql

--- Sql_ICG_projekat_Danilo_Radulovic.sql
+++ Sql_ICG_projekat_Danilo_Radulovic.sql
@@ -16,7 +16,6 @@ create table dbo.projekti(
  id_proj smallint not null primary key,
  ime_proj nchar(30) not null,
  id_sekt smallint foreign key references sektor(id_sekt),
- id_radnika smallint foreign key references radnik(id_radnika),
  pocetak_proj datetime,
  kraj_proj datetime,
 );
@@ -29,5 +28,3 @@ create table dbo.radnik(
 	id_proj smallint foreign key references projekti(id_proj),
 	dat_zap datetime,
 );
-alter table dbo.projekti
-add foreign key (id_radnika) references radnik(id_radnika);
